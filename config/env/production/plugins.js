02bb6b0f1cf8165e0dc604c739dabd1a84ad6913
Lucia Moral <lmoralbaena@gmail.com>
2023-05-01 11:13:46 +0200

    configured cloudinary to strapi

---
+++ config/env/production/plugins.js
@@ -0,0 +1,19 @@
+module.exports = ({ env }) => ({
+
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: "dtwo70uhi",
+        api_key: "435772781929919",
+        api_secret: "GOXKr9sVgS6g2pT8dbFXvhncun8",
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+
+});
